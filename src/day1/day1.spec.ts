c40598eae414c1993b0a9c88179dc8a06e3d39e8
Ben Mrejen <benm@veriti.ai>
2023-12-06 00:08:54 +0100

    day 1 tests passing

--- src/day1/day1.spec.ts
+++ src/day1/day1.spec.ts
@@ -1,5 +1,63 @@
-describe("Day 1", () => {
-  it("should be true", () => {
-    expect(1).toBe(1);
+import {
+  deleteNonNumbersFromString,
+  mapStringToNumbersArray,
+  keepFirstAndLastElement,
+  sumFirstAndLastElements,
+  sumNumbers
+} from "./day1.ts";
+import { names } from "./names.ts";
+
+describe("general test", () => {
+  it("should test removeAlphaNumerical", () => {
+    const str = "abc123def456";
+    const result = deleteNonNumbersFromString(str);
+    expect(result).toBe("123456");
+  });
+
+  it("should test mapStringToNumber", () => {
+    const str = "123456";
+    const result = mapStringToNumbersArray(str);
+    expect(result).toStrictEqual([1, 2, 3, 4, 5, 6]);
+  });
+
+  it("should keep first and last element of array", () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const result = keepFirstAndLastElement(arr);
+    expect(result).toStrictEqual([1, 6]);
+  });
+
+  it("should keepFirstAndLastElement if there is only one", () => {
+    const arr = [1];
+    const result = keepFirstAndLastElement(arr);
+    expect(result).toStrictEqual([1, 1]);
+  });
+
+  it("should work even without numbers", () => {
+    const res = keepFirstAndLastElement(["a"]);
+    expect(res).toStrictEqual(["a", "a"]);
+  });
+
+  it("should throw on empty array", () => {
+    const arr: number[] = [];
+    expect(() => {
+      keepFirstAndLastElement(arr);
+    }).toThrow();
+  });
+
+  it("should sum these two elements", () => {
+    const arr: [number, number] = [1, 6];
+    const result = sumFirstAndLastElements(arr);
+    expect(result).toBe(16);
+  });
+
+  it("should iterate on names", () => {
+    const listOfNumbers = names.map((name) => {
+      const nameWithoutLetters = deleteNonNumbersFromString(name);
+      const nameAsArray = mapStringToNumbersArray(nameWithoutLetters);
+      const [first, last] = keepFirstAndLastElement(nameAsArray);
+      return sumFirstAndLastElements([first, last]);
+    });
+    const sum = sumNumbers(listOfNumbers);
+    expect(sum).toBe(53921);
   });
 });
---
+++ src/day1/day1.ts
@@ -0,0 +1,24 @@
+export function deleteNonNumbersFromString(str: string): string {
+  return str.replace(/[^0-9]/g, "");
+}
+
+export function mapStringToNumbersArray(str: string): number[] {
+  return str.split("").map(deleteNonNumbersFromString).map(Number);
+}
+
+export function keepFirstAndLastElement<T, U>(arr: (T | U)[]): [T | U, T | U] {
+  if (arr.length === 0) {
+    throw new Error("arr is empty");
+  }
+  const first = arr[0];
+  const last = arr[arr.length - 1] || first;
+  return [first, last];
+}
+
+export function sumFirstAndLastElements(arr: [number, number]): number {
+  return Number(String(arr[0]) + String(arr[1]));
+}
+
+export function sumNumbers(arr: number[]): number {
+  return arr.reduce((a, b) => a + b, 0);
+}
